d17202af80cf88d2b83db3b88659cef5a0b3fea3
Rosario Raulin <post@rosario-raulin.de>
2014-01-18 15:33:40 +0100

    Initial Commit

---
+++ RegularExpressions/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RegularExpressions
+//
+//  Created by Rosario Raulin on 18.01.14.
+//  Copyright (c) 2014 Rosario Raulin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
